dabb41bab2af3fda5997c77cc8836d752455b18d
Yarriba Castro <yarribacastro@Yarribas-MacBook-Pro.local>
2020-05-06 21:55:59 -0500

    Server running

---
+++ index.js
@@ -0,0 +1,10 @@
+// Similar to import express from express
+const express = require("express");
+
+const server = express();
+
+const PORT = 5000;
+
+server.listen(PORT, () =>
+  console.log(`\n *** API on http://localhost:${PORT} ***\n`)
+);
